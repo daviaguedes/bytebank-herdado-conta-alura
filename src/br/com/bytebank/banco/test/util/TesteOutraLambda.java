49e5e9bef408d87adbe6642e1a954af5e5149647
daviaguedes <77637069+daviaguedes@users.noreply.github.com>
2021-06-28 11:28:12 -0300

    Java.util Estudo
    Lambda no java.util

--- src/br/com/bytebank/banco/test/util/TesteOutraLambda.java
+++ src/br/com/bytebank/banco/test/util/TesteOutraLambda.java
@@ -54,7 +54,6 @@ public class TesteOutraLambda {
 		System.out.println("-------------------------------");
 
 		lista.forEach((conta) -> System.out.println(conta + ", " + conta.getTitular().getNome())); // lambda
-
 	}
 
 }
